aa345992428d1e701fdbdedb31bf843c236ddc13
Paul Oladele <pauloladele@live.co.uk>
2018-09-02 21:11:32 +0100

    adjusting a few things

--- src/main/resources/public/js/UserAccountTemplateScript.js
+++ src/main/resources/public/js/UserAccountTemplateScript.js
@@ -626,7 +626,7 @@ function logOutWarrior() {
 
     fetch(url, {
         method:'PUT',
-        body: JSON.stringify(data),
+//        body: JSON.stringify(data),
         headers:{
             'Content-Type': 'application/json'
         }
