6190cf93c33efc4653a1f39730e5468e9fa0aac5
Paul Oladele <pauloladele@live.co.uk>
2018-09-02 21:41:53 +0100

    adjusting a few things

--- src/main/java/com/example/Controller/LogOutController.java
+++ src/main/java/com/example/Controller/LogOutController.java
@@ -1,13 +1,12 @@
 package com.example.Controller;
 
 import com.example.Models.Other.LocationPrinter;
+import com.example.Models.Warrior.Warrior;
 import com.example.Services.DaoServices.WarriorDaoServiceImplementation;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Map;
@@ -18,12 +17,14 @@ public class LogOutController {
     WarriorDaoServiceImplementation warriorDaoServiceImplementation = new WarriorDaoServiceImplementation();
 
 
-    @RequestMapping(value="/logout/{name}", method= RequestMethod.PUT)
+    @RequestMapping(value="/logout/{name}", method = RequestMethod.PUT)
     public void logout(@RequestBody Map<String, String> userDetails, org.apache.catalina.servlet4preview.http.HttpServletRequest req, HttpServletResponse res)throws IOException {
         LocationPrinter.printLocation("LOGOUT");
         String[] servletPath = req.getServletPath().split("/");
         String name = servletPath[2];
-        warriorDaoServiceImplementation.logout(name);
+        Warrior warrior = (Warrior) warriorDaoServiceImplementation.getObject(name);
+        warrior.setOnline(false);
+        warriorDaoServiceImplementation.updateObject(name, warrior);
 //        String clients_username = userDetails.get("username");
 //        String clients_password = userDetails.get("password");
 //        System.out.println("PASSWORD " + clients_password);
